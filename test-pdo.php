928a67539309b25915fec62dd83d4c4005323832
seb <contact@smaloron.eu>
2021-06-01 12:34:42 +0200

    test des requêtes SELECT sur citations

--- test-pdo.php
+++ test-pdo.php
@@ -5,7 +5,40 @@ require "lib/pdo.php";
 // Test de la connexion
 try {
     $connexion = getPDO();
-    echo "ça marche";
+    echo "ça marche <br>";
+
+    // Requête sur la base de données
+    $sql = "SELECT * FROM citations";
+    $query = $connexion->query($sql);
+   
+
+    // Récupération des données ligne à ligne
+    $data = $query->fetch();
+    var_dump($data);
+    
+    echo "<br>";
+
+    $data = $query->fetch();
+    var_dump($data);
+
+    // Récupération de toutes les données
+    echo "<pre>";
+    $data = $query->fetchAll(PDO::FETCH_ASSOC);
+
+    var_dump($data);
+
+    echo "</pre>";
+
+    // Afficher les données de $data (juste la colonne texte) dans une liste à puce
+
+    echo "<ul>";
+
+    foreach($data as $citation){
+        echo "<li>". $citation['texte']. "</li>";
+    }
+
+    echo "</ul>";
+
 } catch(Exception $error){
-    echo "ça plante";
+    echo $error->getMessage();
 }
